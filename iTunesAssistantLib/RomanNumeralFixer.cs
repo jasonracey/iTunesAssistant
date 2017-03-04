1fc16958d82ab2b1cf6cb725846a935a1cf60ce4
Jason Racey <jasonracey@yahoo.com>
2017-03-03 19:03:51 -0800

    hack to fix roman numerals after title casing track names

---
+++ iTunesAssistantLib/RomanNumeralFixer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace iTunesAssistantLib
+{
+    public static class RomanNumeralFixer
+    {
+        public static string FixRomanNumerals(this string s)
+        {
+            s = s.Replace("Ii.", "II.");
+            s = s.Replace("Iii.", "III.");
+            s = s.Replace("Iv.", "IV.");
+            return s;
+        }
+    }
+}
