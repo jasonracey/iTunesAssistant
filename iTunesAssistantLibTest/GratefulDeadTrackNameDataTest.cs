6c1a35e608e46fc82e3d7611a3f7208fda87b296
Jason Racey <jasonracey@gmail.com>
2020-09-14 22:08:38 -0700

    Move GD track name search into separate class and add tests

---
+++ iTunesAssistantLibTest/GratefulDeadTrackNameDataTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using iTunesAssistantLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace iTunesAssistantLibTest
+{
+    [TestClass]
+    public class GratefulDeadTrackNameDataTest
+    {
+        [DataTestMethod]
+        [DataRow("", null)]
+        [DataRow(" ", null)]
+        [DataRow("blah", null)]
+        [DataRow("alligator", "Alligator")]
+        [DataRow(" penelope  alligator armadillo  ", "Alligator")]
+        public void Test(string term, string expected)
+        {
+            GratefulDeadTrackNameData.Search(term).Should().Be(expected);
+        }
+    }
+}
