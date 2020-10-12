f63b8e7912890f78a07a76ec5fcf67b0dbd9acf2
Jason Racey <jasonracey@gmail.com>
2020-10-12 03:34:41 -0700

    Change VS edition in deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,3 @@
-$env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin"
+$env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin"
 MSBuild iTunesAssistant.sln -t:Rebuild -p:Configuration=Release
 xcopy /I /Y .\iTunesAssistant\bin\Release\netcoreapp3.1 ..\..\Tools\iTunesAssistant
